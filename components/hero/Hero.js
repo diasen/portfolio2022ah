7fe6cf403504e9424979cb48d565d06826916293
Alexander Hansen <Alexander-hansen@hotmail.com>
2022-02-14 16:49:35 +0100

    image

--- components/hero/Hero.js
+++ components/hero/Hero.js
@@ -1,18 +1,15 @@
 import React from 'react';
 import { HeroSection } from './style';
-import Image from 'next/image';
 
 function Hero() {
   return (
     <HeroSection>
       <div className='imgContainer'>
-        <Image
-          height={120}
-          width={150}
+        <img
           alt='Porfile picture'
           src='/images/profilepic.JPG'
           className='profilePic'
-        ></Image>
+        ></img>
       </div>
       <h1>Hi, I’m Alexander.</h1>
       <p>I'm currently completing my Front-end development degree at Noroff.</p>
